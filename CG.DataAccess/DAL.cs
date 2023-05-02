8728a1cbaf3e6e06fb7c085876330c43293659b2
Chet Gray <chet.gray@waystar.com>
2023-05-02 17:19:06 -0400

    Add project files.

---
+++ CG.DataAccess/DAL.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CG.DataAccess
+{
+    /// <inheritdoc/>
+    public class DAL : IDAL
+    {
+        private readonly string _connectionString;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DAL"/> class with the passed <paramref
+        /// name="connectionString"/>.
+        /// </summary>
+        /// <param name="connectionString">
+        /// The connection string to use for any <see cref="SqlConnection"/>.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when the passed <paramref name="connectionString"></paramref> is <see
+        /// langword="null"/>.
+        /// </exception>
+        public DAL(string connectionString)
+        {
+            _connectionString =
+                connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public void ExecuteStoredProcedure(
+            string storedProcedureName,
+            IDictionary<string, object> parameters
+        )
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (
+                SqlCommand command = new SqlCommand(storedProcedureName, connection)
+                {
+                    CommandType = CommandType.StoredProcedure,
+                }
+            )
+            {
+                connection.Open();
+                foreach (KeyValuePair<string, object> parameter in parameters)
+                {
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                }
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public object GetValueFromStoredProcedure(
+            string storedProcedureName,
+            IDictionary<string, object> parameters
+        )
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (
+                SqlCommand command = new SqlCommand(storedProcedureName, connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                }
+            )
+            {
+                connection.Open();
+                foreach (KeyValuePair<string, object> parameter in parameters)
+                {
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                }
+                return command.ExecuteScalar();
+            }
+        }
+
+        public DataTable GetTableFromStoredProcedure(
+            string storedProcedureName,
+            IDictionary<string, object> parameters
+        )
+        {
+            DataTable resultTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (
+                SqlCommand command = new SqlCommand(storedProcedureName, connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                }
+            )
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                connection.Open();
+                foreach (KeyValuePair<string, object> parameter in parameters)
+                {
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                }
+                // Get the first DataTable in the result DataSet
+                adapter.Fill(resultTable);
+            }
+            return resultTable;
+        }
+    }
+}
---
+++ CG.DataAccess/IDAL.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace CG.DataAccess
+{
+    /// <summary>The data access between the data layer and the database.</summary>
+    public interface IDAL
+    {
+        /// <summary>Executes a stored procedure with parameters.</summary>
+        /// <param name="storedProcedureName">
+        /// The name of the stored procedure to execute.
+        /// </param>
+        /// <param name="parameters">
+        /// A dictionary of the parameters to pass to the stored procedure.
+        /// </param>
+        void ExecuteStoredProcedure(
+            string storedProcedureName,
+            IDictionary<string, object> parameters
+        );
+
+        /// <inheritdoc cref="ExecuteStoredProcedure"/>
+        /// <summary>
+        /// Executes a stored procedure with parameters, returning a single value.
+        /// </summary>
+        /// <returns>The single scalar value returned from the stored procedure.</returns>
+        object GetValueFromStoredProcedure(
+            string storedProcedureName,
+            IDictionary<string, object> parameters
+        );
+
+        /// <inheritdoc cref="ExecuteStoredProcedure"/>
+        /// <summary>
+        /// Executes a stored procedure with parameters, returning a <see cref="DataTable"/>.
+        /// </summary>
+        /// <returns>
+        /// The first <see cref="DataTable"/> in the result set returned from the stored
+        /// procedure.
+        /// </returns>
+        DataTable GetTableFromStoredProcedure(
+            string storedProcedureName,
+            IDictionary<string, object> parameters
+        );
+    }
+}
